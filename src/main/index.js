d30c1389b0a82799d8427371f19c286c0e1b431c
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-03-03 15:22:14 +0000

    change to hash routing due to server restrictions

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import Footer from '../footer'
 import Header from '../header'
 import Routing from '../routing';
