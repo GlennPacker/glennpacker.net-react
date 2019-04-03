422f06e48254e6e8136c38ad63377255089e3474
glennpacker <glenn.packer@branthill.co.uk>
2019-04-03 09:42:05 +0100

    client load hookup to page

--- src/components/home/clients/index.js
+++ src/components/home/clients/index.js
@@ -1,46 +1,66 @@
-import React, { Component } from 'react';
-import Client from '../client'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import Client from "../client";
+import * as clientActions from "../../../redux/actions/clientActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { Style } from 'radium'
-import styles from './clientsStyle'
+import { Style } from "radium";
+import styles from "./clientsStyle";
 
 class Clients extends Component {
-	render() {
-		if (this.props.clients) {
-			return (
-				<div className="card">
-					<Style rules={styles} />
-					<div className="card-header bg-info text-white">
-						Clients
-					</div>
-					<div className="card-body client-card-body">
-						<p className="card-text">Previous clients in recent years:</p>
-						<ul>
-							{ this.props.clients.map((client, i) => {
-								return (<Client key={i} client={client} />)
-							})}
-						</ul>
-					</div>
-				</div>
-			)
-		} else {
-			return (null)
-		}
-	}
+  componentDidMount() {
+    const { actions, clients } = this.props;
+
+    if (clients.length === 0) {
+      actions.loadClients().catch(error => {
+        alert("Loading courses failed" + error);
+      });
+    }
+  }
+
+  render() {
+    if (this.props.clients) {
+      return (
+        <div className="card">
+          <Style rules={styles} />
+          <div className="card-header bg-info text-white">Clients</div>
+          <div className="card-body client-card-body">
+            <p className="card-text">Previous clients in recent years:</p>
+            <ul>
+              {this.props.clients.map((client, i) => {
+                return <Client key={i} client={client} />;
+              })}
+            </ul>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 Clients.propTypes = {
-	clients: PropTypes.array.isRequired
+  actions: PropTypes.object.isRequired,
+  clients: PropTypes.array.isRequired
 };
 
-function mapStateToProps (state) {
-	return {
-		clients: state.clients
-	}
+function mapStateToProps(state) {
+  return {
+    clients: state.clients
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      loadClients: bindActionCreators(clientActions.loadClients, dispatch)
+    }
+  };
 }
 
 export default connect(
-	mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Clients);
