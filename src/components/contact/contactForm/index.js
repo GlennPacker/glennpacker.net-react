bd862f69356b447416aa905e750c7b5bddcddd8a
glennpacker <glenn.packer@branthill.co.uk>
2019-04-04 08:07:15 +0100

    improved user experience on sending a contact message

--- src/components/contact/contactForm/index.js
+++ src/components/contact/contactForm/index.js
@@ -1,56 +1,61 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const ContactForm = (props) => {
-    const contactDetails = props.contactDetails;
-    const sendForm = props.submitForm;
-    const onFieldChange = props.onFieldChange;
-
-    return (
-        <div className="container pt-3">
-            <form onSubmit={sendForm}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                        required
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter name"
-                        name="name"
-                        value={contactDetails.name}
-                        onChange={onFieldChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="email">Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                        name="email"
-                        value={contactDetails.email}
-                        onChange={onFieldChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        required
-                        className="form-control"
-                        placeholder="Enter message"
-                        rows="3"
-                        name="message"
-                        value={contactDetails.message}
-                        onChange={onFieldChange}
-                    />
-                </div>
-
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+function ContactForm({ contactDetails, submitForm, onFieldChange, sending }) {
+  return (
+    <div className="container pt-3">
+      <form onSubmit={submitForm}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            required
+            type="text"
+            className="form-control"
+            placeholder="Enter name"
+            name="name"
+            value={contactDetails.name}
+            onChange={onFieldChange}
+          />
         </div>
-    )
+
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            name="email"
+            value={contactDetails.email}
+            onChange={onFieldChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            required
+            className="form-control"
+            placeholder="Enter message"
+            rows="3"
+            name="message"
+            value={contactDetails.message}
+            onChange={onFieldChange}
+          />
+        </div>
+
+        <button disabled={sending} type="submit" className="btn btn-primary">
+          {sending ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </div>
+  );
 }
 
+ContactForm.propTypes = {
+  contactDetails: PropTypes.object.isRequired,
+  submitForm: PropTypes.func.isRequired,
+  onFieldChange: PropTypes.func.isRequired,
+  sending: PropTypes.bool.isRequired
+};
 
 export default ContactForm;
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,59 +1,66 @@
 import React, { Component } from "react";
-import ContactForm from './contactForm';
-import ConstactSubmitted from './contactSubmited';
-import ServerProblems from './serverProblems';
-import axios from 'axios';
+import ContactForm from "./contactForm";
+import ConstactSubmitted from "./contactSubmited";
+import ServerProblems from "./serverProblems";
+import axios from "axios";
 
 class Contact extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			contactDetails: {
-				name: '',
-				email: '',
-				message: ''
-			},
-			submitted: false,
-			serverFail: false
-		}
-		this.onFieldChange = this.onFieldChange.bind(this);
-		this.sendContactDetails = this.sendContactDetails.bind(this)
-	}
+    this.state = {
+      sending: false,
+      contactDetails: {
+        name: "",
+        email: "",
+        message: ""
+      },
+      submitted: false,
+      serverFail: false
+    };
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.sendContactDetails = this.sendContactDetails.bind(this);
+  }
 
-	onFieldChange(event) {
-		this.setState({
-			contactDetails: {
-				...this.state.contactDetails,
-				[event.target.name]: event.target.value
-			}
-		});
-	}
+  onFieldChange(event) {
+    this.setState({
+      contactDetails: {
+        ...this.state.contactDetails,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
 
-	sendContactDetails(event) {
-		axios.post('http://glennpacker.net/api/contact', this.state.contactDetails).then(() => {
-			this.setState({submitted: true})
-		}).catch(() => {
-			this.setState({serverFail: true})
-		})
+  sendContactDetails(event) {
+    debugger;
+    this.setState({ ...this.state, sending: true });
 
-		event.preventDefault()
-	}
+    axios
+      .post("http://glennpacker.net/api/contact", this.state.contactDetails)
+      .then(() => {
+        this.setState({ submitted: true, sending: false });
+      })
+      .catch(() => {
+        this.setState({ serverFail: true, sending: false });
+      });
 
-	render() {
-		return (
-			!this.state.submitted? 
-				<ContactForm 
-					contactDetails={this.state.contactDetails} 
-					submitForm={this.sendContactDetails} 
-					onFieldChange={this.onFieldChange} 
-				/>
-			:this.state.serverFail ?
-				<ServerProblems />
-			:
-				<ConstactSubmitted />
-		)
-	}
+    event.preventDefault();
+  }
+
+  render() {
+    return !this.state.submitted ? (
+      <ContactForm
+        contactDetails={this.state.contactDetails}
+        submitForm={this.sendContactDetails}
+        onFieldChange={this.onFieldChange}
+        sending={this.state.sending}
+      />
+    ) : this.state.serverFail ? (
+      <ServerProblems />
+    ) : (
+      <ConstactSubmitted />
+    );
+  }
 }
 
-export default Contact
+export default Contact;
