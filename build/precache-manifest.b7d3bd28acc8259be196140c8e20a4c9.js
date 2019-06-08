2926db7eede27f3cf24d4459545b10950a270b73
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-06-08 17:07:00 +0100

    build

---
+++ build/precache-manifest.b7d3bd28acc8259be196140c8e20a4c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e06e7604bf7967a8095ff3dc980d6fa",
+    "url": "/static/media/GlennPacker.2e06e760.jpg"
+  },
+  {
+    "revision": "47ec47200e4b7d60ce51",
+    "url": "/static/css/main.07372200.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82ef8836c05d6f248fc52d53258dfd50",
+    "url": "/static/media/be.82ef8836.png"
+  },
+  {
+    "revision": "ff76847c00891510b366",
+    "url": "/static/js/2.00b8af11.chunk.js"
+  },
+  {
+    "revision": "47ec47200e4b7d60ce51",
+    "url": "/static/js/main.090074ec.chunk.js"
+  },
+  {
+    "revision": "6b73305c4027f8c8f832d191820dd08c",
+    "url": "/static/media/cat-waiting-for-the-mail.6b73305c.jpg"
+  },
+  {
+    "revision": "2af6674ef47dc6180193302baac22c98",
+    "url": "/static/media/spa.2af6674e.png"
+  },
+  {
+    "revision": "98e4378ed93fabeafbac2d7444d86d11",
+    "url": "/static/media/fe.98e4378e.png"
+  },
+  {
+    "revision": "ff76847c00891510b366",
+    "url": "/static/css/2.476d0211.chunk.css"
+  },
+  {
+    "revision": "64067f30d89ac3d47fc815a87aeb1a6c",
+    "url": "/index.html"
+  }
+];
